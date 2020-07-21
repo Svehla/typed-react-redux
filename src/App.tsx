aafcdffc049475a2345b4a77718f6ea4c8755a3f
Jakub Švehla <svehl.jakub@gmail.com>
2020-07-21 15:52:44 +0200

    add basic redux ts example;

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,110 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createStore, applyMiddleware, Action } from 'redux'
+import { useSelector, Provider, useDispatch } from 'react-redux'
+import { combineReducers } from 'redux';
+import thunk, { ThunkAction } from 'redux-thunk';
+
+// helper generics
+type GetStateFromReducers<T extends ((...args: any[]) => any) | { [key: string]: any }> = {
+  [K in keyof T]: T[K] extends ((...args: any[]) => any)
+    ? ReturnType<T[K]>
+    : GetStateFromReducers<T[K]>
+}
+
+type ThunkReturnType <ActionT extends Action<string>, ReturnType=Promise<ActionT> | Promise<void> | void> =
+  ThunkAction<ReturnType, GlobalState, void, ActionT>
+
+// ---------------
+// ---- utils ----
+const delay = (timeout: number) => new Promise(resolve => setTimeout(resolve, timeout))
+
+// ---------------
+// counter reducer
+// ---------------
+const INCREMENT = 'INCREMENT' as const
+const DECREMENT = 'DECREMENT' as const
+
+const increment = () => ({
+  type: INCREMENT,
+})
+
+const decrement = () => ({
+  type: DECREMENT,
+})
+
+type ActionType = 
+  | ReturnType<typeof increment>
+  | ReturnType<typeof decrement>
+
+type IncrementThunk = ThunkReturnType<ActionType>
+
+
+export const asyncIncrement = (timeout: number): IncrementThunk =>
+  async (dispatch, _getState) => {
+    await delay(timeout)
+    dispatch(increment())
+    await delay(timeout)
+    dispatch(increment())
+  };
+  
+  
+const defaultState = {
+  value: 0
+}
+type State = typeof defaultState
+
+function counter(state = defaultState, action: ActionType): State {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, value: state.value + 1 }
+    case DECREMENT:
+      return { ...state, value: state.value - 1 }
+    default:
+      return state
+  }
+}
+
+// ----------------
+// --- app init ---
+// ----------------
+const reducers = {
+  // TODO: add reducers
+  counter
+};
+// Create a Redux store holding the state of your app.
+// Its API is { subscribe, dispatch, getState }.
+let store = createStore(combineReducers(reducers), applyMiddleware(thunk))
+
+type GlobalState = GetStateFromReducers<typeof reducers>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Counter />
+    </Provider>
   );
+}
+
+// ----- sub component
+const getCounterValue = (state: GlobalState) => state.counter.value
+
+const Counter = () => {
+  const dispatch = useDispatch()
+  const counterValue = useSelector(getCounterValue)
+
+  const increment = () => { dispatch(increment()) }
+  const decrement = () => { dispatch(decrement()) }
+
+  return (
+    <div>
+      {counterValue}
+      <button onClick={increment}>+</button>
+      <button onClick={decrement}>-</button>
+      <button onClick={() => { dispatch(asyncIncrement(200))}}>make 200ms async increment</button>
+      <button onClick={() => { dispatch(asyncIncrement(400))}}>make 400ms async increment</button>
+      <button onClick={() => { dispatch(asyncIncrement(800))}}>make 800ms async increment</button>
+    </div>
+  )
 }
 
 export default App;
